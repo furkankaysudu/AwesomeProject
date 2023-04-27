5b3cc3ddd172f184b150c2b44cde3ce18ad7f673
furkankaysudu <furkankaysudu@gmail.com>
2023-04-27 14:16:24 +0300

    calculate point-checkWords-cleanerStart/uncomplete

--- App.js
+++ App.js
@@ -6,12 +6,12 @@ const App = () => {
   const [selectedLetters, setSelectedLetters] = useState([]);
   const baseUrl = "https://sozluk.gov.tr/gts?ara=";
   const [control, setControl] = useState(false);
-  const kelimeDogru= '';
+  const [yanlisDeneme, setYanlisDeneme] = useState(0);
 
 
   useEffect(() => {
     // Türkçe alfabeden rastgele harfler seçin
-    const alphabet = "abcçdefgğhıijklmnoöprsştuüvyz";
+    const alphabet = "ABCÇDEFGĞHIİJKLMNOÖPRSŞTUÜVYZ";
     const selectedLetters = [];
     for (let i = 0; i < 24; i++) {
       selectedLetters.push(alphabet[Math.floor(Math.random() * alphabet.length)]);
@@ -29,10 +29,10 @@ const App = () => {
   const [point, setPoint] = useState(0);
   function calculatePoints(kelime) {
     const scores = {
-      'a': 1, 'b': 3, 'c': 4, 'ç': 4, 'd': 3, 'e': 1, 'f': 7, 'g': 5,
-      'ğ': 8, 'h': 5, 'ı': 2, 'i': 2, 'j': 10, 'k': 1, 'l': 1, 'm': 2,
-      'n': 1, 'o': 2, 'ö': 7, 'p': 5, 'r': 1, 's': 2, 'ş': 4, 't': 1,
-      'u': 2, 'ü': 3, 'v': 7, 'y': 3, 'z': 4,
+      'A': 1, 'B': 3, 'C': 4, 'Ç': 4, 'D': 3, 'E': 1, 'F': 7, 'G': 5,
+      'Ğ': 8, 'H': 5, 'I': 2, 'İ': 2, 'J': 10, 'K': 1, 'L': 1, 'M': 2,
+      'N': 1, 'O': 2, 'Ö': 7, 'P': 5, 'R': 1, 'S': 2, 'Ş': 4, 'T': 1,
+      'U': 2, 'Ü': 3, 'V': 7, 'Y': 3, 'Z': 4,
     };
     let point = 0;
     for (let i = 0; i < kelime.length; i++) {
@@ -42,14 +42,13 @@ const App = () => {
       setPoint(point)
     }
     console.log(point)
-    
     return point;
   }
   
-
+  
   const handleStartButtonPress = async () => {
     if(selectedLetters.length>2){
-      const res = await axios.get(baseUrl+word)
+      const res = await axios.get(baseUrl+word.toLowerCase())
       try {
         word === res.data[0].madde
           console.log("true")
@@ -57,6 +56,10 @@ const App = () => {
           setSelectedLetters([]);
         } catch {
         console.log("false");
+        setYanlisDeneme(yanlisDeneme+1)
+        
+        if (point!=0){setPoint(point-1)}
+        
       }
     }
   }
@@ -68,16 +71,34 @@ const App = () => {
   }
   const puanıSıfırla = () =>{
     setPoint(0)
+    setYanlisDeneme(0)
   }
 
   const renderGrid = () => {
     const grid = [];
-    for (let i = 0; i < 10; i++) {  // en alt üç satırı oluşturmak için değişiklik yaptık
+  
+    // İlk 7 satırı boş bırakın
+    for (let i = 0; i < 7; i++) {
+      const emptyRow = [];
+      for (let j = 0; j < 8; j++) {
+        emptyRow.push(
+          <View key={`empty-${i}-${j}`} style={styles.cell}></View>
+        );
+      }
+      grid.push(
+        <View key={i} style={styles.row}>
+          {emptyRow}
+        </View>
+      );
+    }
+  
+    // Son 3 satıra harfleri yerleştirin
+    for (let i = 7; i < 10; i++) {
       const row = [];
       for (let j = 0; j < 8; j++) {
-        const index = i * 8 + j;
+        const index = (i - 7) * 8 + j;
         row.push(
-          <TouchableOpacity 
+          <TouchableOpacity
             key={index}
             onPress={() => handleLetterPress(letters[index])}
             style={styles.cell}
@@ -94,17 +115,24 @@ const App = () => {
     }
     return (
       <>
-        {grid}
-        <TouchableOpacity onPress={handleStartButtonPress} style={styles.button}>
-          <Text style={styles.buttonText}>GÖNDER</Text>
-        </TouchableOpacity>
-        <TouchableOpacity onPress={puanıSıfırla} style={styles.button}>
-          <Text style={styles.buttonText}>SIFIRLA</Text>
-        </TouchableOpacity>
-        <TouchableOpacity onPress={handleClearButtonPress} style={styles.button}>
-          <Text style={styles.buttonText}>TEMİZLE</Text>
-        </TouchableOpacity>
-      </>
+      {grid}
+      <View style={styles.buttonViews}>
+      
+      <TouchableOpacity onPress={handleStartButtonPress} style={styles.button}>
+        <Text style={styles.buttonText}>GÖNDER</Text>        
+      </TouchableOpacity>
+      
+      <TouchableOpacity onPress={puanıSıfırla} style={styles.button}>
+        <Text style={styles.buttonText}>SIFIRLA</Text>
+      </TouchableOpacity>
+      <TouchableOpacity onPress={handleClearButtonPress} style={styles.button}>
+        <Text style={styles.buttonText}>TEMİZLE</Text>
+      </TouchableOpacity>
+      
+      </View>
+      <Text style={styles.selectedLetters}>HATA:{yanlisDeneme}</Text>
+    </>
+    
     );
   }
 
@@ -127,11 +155,17 @@ const App = () => {
 
 const styles = StyleSheet.create({
   container: {
+    marginTop:20,
     flex: 1,
     justifyContent: 'center',
-    alignItems: 'center',
+    
     backgroundColor: '#fff',
   },
+  buttonViews:{
+    flexDirection:'row',
+    justifyContent: 'space-around'
+  }
+  ,
   bottomRow: {
     flexDirection: 'row',
     justifyContent: 'space-between',
@@ -163,7 +197,7 @@ const styles = StyleSheet.create({
     alignItems: 'center',
   },
   cell: {
-    borderWidth: 2,
+    borderWidth: 1,
     borderColor: '#333',
     width: 50,
     height: 50,
@@ -180,7 +214,7 @@ const styles = StyleSheet.create({
     paddingVertical: 10,
     paddingHorizontal: 20,
     borderRadius: 5,
-    marginVertical: 10,
+    marginVertical: 4,
   },
   buttonText: {
     color: '#fff',
